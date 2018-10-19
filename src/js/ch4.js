f8b0c8d82d80abbd7c906e7ef85c15fff5357491
rliang.lai <rliang.lai@outlook.com>
2018-10-19 18:05:11 +0800

    4.2 作为函数直接被调用

--- src/js/ch4.js
+++ src/js/ch4.js
@@ -12,3 +12,63 @@ function whatever(a, b ,c){
 }
 
 whatever(1,2,3,4,5);
+
+function sum(){
+    var sum = 0;
+    for (var i = 0; i < arguments.length; i++){
+        sum += arguments[i];
+    }
+    return sum;
+}
+
+function sum_rest(...restArgs){
+    var sum = 0;
+    for (var i = 0; i < restArgs.length; i++){
+        sum += restArgs[i];
+    }
+    return sum;
+}
+
+assert(sum(1, 2) === 3, 'We can add two numbers');
+assert(sum(1, 2, 3) === 6, 'We can add three numbers');
+assert(sum(1, 2, 3, 4) === 10, 'We can add four numbers');
+assert(sum_rest(1, 2, 3, 4) === 10, 'We can add four numbers using rest parameter');
+
+function infiltrate(person){
+	'use strict';
+
+	assert(person === 'gardener', 'The person is a gardener');
+	assert(arguments[0] === 'gardener', 'The first argument is a gardener');
+
+	arguments[0] = 'ninja';
+
+	assert(person === 'ninja', 'The person is a ninja');
+	assert(arguments[0] === 'ninja', 'The first argument is a ninja');
+
+	person = 'gardener';
+	assert(person === 'gardener', 'The person is a gardener once more');
+	assert(arguments[0] === 'gardener', 'The first argument is a gardener again');
+}
+
+infiltrate('gardener');
+
+function skulk(name){}
+function Ninja(name){}
+
+// 作为函数调用
+skulk('Hattori');
+(function(who){return who;})('Hattori');
+
+var ninja = {
+	skulk: function(){}
+};
+
+// 作为一个方法调用
+ninja.skulk('Hattori');
+
+// 作为构造函数调用
+ninja = new Ninja('Hattori');
+// 通过call方法调用
+skulk.call(ninja, 'Hattori');
+// 通过apply方法调用
+skulk.apply(ninja, ['Hattori']);
